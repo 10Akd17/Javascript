5ecf4765c1953055ba68f789d313126ea2cf26c5
Akd <abdelkarimcherif5@gmail.com>
2021-11-15 16:27:56 +0100

    ok git commit -m ok git commit -m ok

---
+++ exercices/01exo.js
@@ -0,0 +1,9 @@
+var prenom = prompt("Entrez votre prenom");
+
+var nom = prompt("Entrez votre nom");
+
+var phrase = prenom + " " + nom;
+
+alert("Bonjour Mr " + phrase + " comment allez-vous ? ");
+
+document.write(" Bonne continuation Mr" + " " + phrase);
---
+++ js/02_concatenation.js
@@ -0,0 +1,26 @@
+//alert("C'est bon"); on a tester notre chemin avec une fonction alert()
+
+/*--------------
+la concatenation 
+---------------*/ 
+
+var annee = 2017; // variable de type nombre 
+var futur = 5;
+var calcul = annee + futur;
+console.log(calcul);
+document.write(calcul + "<br>"); // affiche le resultat sur la page avec un <br>
+
+var mois = "7" ;
+var calcul2 = annee + mois;
+console.log(calcul2);
+document.write(calcul2 + "<br>");
+
+var debutPhrase = "Aujourdhui ";
+var nbStagiaire = 11;
+var suitePhrase = " stragiaires";
+var finPhrase = "sont present.";
+document.write(debutPhrase + nbStagiaire + suitePhrase + " " + finPhrase);
+//le " " est souvent utilisé lors de la recuperation de données saisies par l'utilisateur
+
+
+
